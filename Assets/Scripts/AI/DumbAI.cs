aebc2fd576b1cbf89517e5ba495b2c3f7858c58c
Martin Grunbaum <martin@itsolveonline.net>
2014-10-02 01:30:58 +0200

    Initial commit

---
+++ Assets/Scripts/AI/DumbAI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PolyNavAgent))]
+public class DumbAI : MonoBehaviour {
+    private GameObject player;
+
+    private PolyNavAgent _agent;
+	public PolyNavAgent agent{
+		get
+		{
+			if (!_agent)
+				_agent = GetComponent<PolyNavAgent>();
+			return _agent;			
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+        this.player = GameObject.FindGameObjectWithTag("Player");
+        InvokeRepeating("AI", 0f, 0.25f);
+	}
+
+    void AI()
+    {
+        this.agent.SetDestination(player.transform.position);
+    }
+
+    //Message from Agent
+    void OnDestinationReached()
+    {
+
+        //do something here...
+    }
+
+    //Message from Agent
+    void OnDestinationInvalid()
+    {
+
+    }
+
+	void Update () {
+	    
+	}
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Provides basic WSAD movement, hooking into an Animator
+/// which expects XMovement and YMovement to change depending
+/// on which direction the player is facing/moving in.
+/// 
+/// Up/Down movement takes priority, so moving diagonally will always
+/// show an up or down animation.
+/// </summary>
+public class Movement : MonoBehaviour {
+    [SerializeField]
+    private float movementSpeed = 1f;
+
+    private Animator animator;
+
+    void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+	void FixedUpdate () {
+        Vector2 vel = new Vector3();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetFloat("XMovement", -1f);
+            animator.SetFloat("YMovement", 0f);
+            vel += new Vector2(-1, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetFloat("XMovement", 1f);
+            animator.SetFloat("YMovement", 0f);
+            vel += new Vector2(1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat("YMovement", 1f);
+            animator.SetFloat("XMovement", 0f);
+            vel += new Vector2(0, 1);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetFloat("YMovement", -1f);
+            animator.SetFloat("XMovement", 0f);
+            vel += new Vector2(0, -1);
+        }
+
+        if (vel.magnitude > 0.001)
+        {
+            animator.SetBool("Walking", true);
+            Vector3.Normalize(vel);
+            vel *= this.movementSpeed;
+            rigidbody2D.velocity = vel;
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+	}
+}
